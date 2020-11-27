9bc722b56ee1964d23f3adbad3b3c58d07c4ba9b
Mikael Sharify-funk <mikaelsharifyfunk@gmail.com>
2020-11-27 08:56:36 -0500

    Doctor Project Complete

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 def welcome():
     print("\nHello! My name is The Doctor. What seems to be the problem?")
+    symtom = input("\nYour symtoms: ")
+    return symtom
 
-def handleSymtoms():
+def handleData():
+    database = []
     try:
-        symtom = input("Your symtoms: ")
         with open('data.txt') as file:
-            database = []
             for line in file:
-                line = data.readline()
-                datag = line.split('-->')
-                print(datag)
-                datasymtom = datag[0].strip()
-                diagnosis = datag[1].strip()
+                data = line.split('-->')
+                datasymtom = data[0].strip()
+                diagnosis = data[1].strip()
                 database.append([datasymtom, diagnosis])
-            for item in database:
-                if datasymtom in symtom:
-                    print("Yay")
-    except:
-        print("Error!")
+    except Exception as e:
+        print(f"Error: {e}")
+    return database
             
-
-    
-    
-   
+def compareSymtoms(symtom, database):
+    found = False
+    for item in database:
+        datasymtom = item[0]
+        diagnosis = item[1]
+        if datasymtom.lower() in symtom.lower():
+            found = True
+            print(f"\nYou have {diagnosis.lower()}.")
+    if not found:
+        print("\nI could not find your symptom.")
+    print("\nThank you for using the doctor.\n")
 
 def main():
-    welcome()
-    handleSymtoms()
-
+    symtom = welcome()
+    database = handleData()
+    compareSymtoms(symtom,database)
 
 if __name__ == "__main__":
     main()
